82fc539697422c029158602f7e0f300e27ab7e3c
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-27 11:34:33 +0600

    alhamdulilah done

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,45 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const Slider: React.FC = () => {
+    const slider = [
+        { _id: 1, title: 'slide 1', description: 'swipper slider description 1' },
+        { _id: 2, title: 'slide 2', description: 'swipper slider description 2' },
+        { _id: 3, title: 'slide 3', description: 'swipper slider description 3' },
+        { _id: 4, title: 'slide 4', description: 'swipper slider description 4' },
+        { _id: 5, title: 'slide 5', description: 'swipper slider description 5' },
+        { _id: 6, title: 'slide 6', description: 'swipper slider description 6' },
+        { _id: 7, title: 'slide 7', description: 'swipper slider description 7' },
+        { _id: 8, title: 'slide 8', description: 'swipper slider description 8' },
+        { _id: 9, title: 'slide 9', description: 'swipper slider description 9' },
+        { _id: 10, title: 'slide 10', description: 'swipper slider description 10' },
+        { _id: 11, title: 'slide 11', description: 'swipper slider description 11' },
+        { _id: 12, title: 'slide 13', description: 'swipper slider description 12' }
+    ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+      <div>
+        <section>
+            <div className='max-w-[1320px] mx-auto my-20'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3'>
+                    {slider.map((slide) => (
+                        <div key={slide._id} className='bg-red-400 text-white p-2 rounded'>
+                            <div className='relative p-2'>
+                                <div className='border-box relative overflow-hidden'>
+                                    <div className='animated-border'>
+                                        <h2 className='text-4xl text-center capitalize'>{slide.title}</h2>
+                                        <p className='text-2xl text-center mt-10 capitalize'>{slide.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+        
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    );
+};
+
+export default Slider;
